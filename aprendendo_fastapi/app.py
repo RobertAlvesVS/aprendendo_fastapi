141010436f8a2f6975e7a29416f2d45b3d608191
Robert Alves <robertalvesvs@gmail.com>
2025-07-08 18:32:53 -0300

    Continuar em casa os estudos

--- aprendendo_fastapi/app.py
+++ aprendendo_fastapi/app.py
@@ -1,7 +1,10 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
 
+from aprendendo_fastapi.database import get_session
+from aprendendo_fastapi.models import User
 from aprendendo_fastapi.schemas import (
     Message,
     UserDB,
@@ -20,16 +23,43 @@ def read_root():
     return {'message': 'Olá Mundo'}
 
 
-@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
-    database.append(user_with_id)
-    return user_with_id
+@app.post(
+    '/users/',
+    status_code=HTTPStatus.CREATED,
+    response_model=UserPublic,
+)
+def create_user(user: UserSchema, session=Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
+
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT, detail='Email already exists'
+            )
+
+    db_user = User(
+        username=user.username, password=user.password, email=user.email
+    )
+
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
 
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
-def read_users():
-    return {'users': database}
+def read_users(session=Depends(get_session), limit: int = 10, offset: int = 0):
+    users = session.scalars(select(User).limit(limit).offset(offset))
+    return {'users': users}
 
 
 @app.put(
